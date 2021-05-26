2596c6d4260eb4d3370ba11f34011f31886a66d1
Devyn Barron <52061626+barrd001@users.noreply.github.com>
2021-05-26 00:41:17 -0700

    initializing

---
+++ app.js
@@ -0,0 +1,8 @@
+// 1.	I want land.
+// 2.	I want an NFT.
+// 3.	I want to create an NFT.
+// 4.	I want to invest.
+// 5.	I want help with building.
+// 6.	I want help with marketing in the metaverse.
+// 7.	I want help making my dreams reality.
+// 8.	I want to do real world good.
